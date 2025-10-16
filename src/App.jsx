df4dcdabd49d47c0d64a192d3229692970ec2985
Ali Mora <56822813+AliMora83@users.noreply.github.com>
2025-10-16 16:37:14 +0200

    App loads Home page by default

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-
+import Home from './pages/Home'
 
 function App() {
-
   return (
-    <>
-      <h1
-        className="text-3xl text-center text-red-700"
-      >Welcome to Vite with TailwindCSS and React</h1>
-    </>
+    <Home />
   )
 }
 
