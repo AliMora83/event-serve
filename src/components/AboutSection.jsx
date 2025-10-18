e2581c906f5145c31cc15b07f34386c9dc07e287
Ali Mora <56822813+AliMora83@users.noreply.github.com>
2025-10-18 17:26:56 +0200

    Add EventLogo.png to AboutSection

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -24,7 +24,9 @@ const AboutSection = () => {
               Learn More About Us
             </button>
           </div>
-          <div className="bg-gradient-to-br from-primary to-primary/80 h-96 rounded-xl shadow-2xl"></div>
+          <div className="bg-gradient-to-br from-primary to-primary/80 h-96 rounded-xl shadow-2xl flex items-center justify-center">
+            <img src='/assets/EventLogo.png' alt='Event Logo' className='mx-auto my-8 rounded-lg shadow-lg w-64 h-auto' />
+          </div>
         </div>
       </div>
     </section>
