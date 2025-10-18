81325574034a4e64389b3c7747f7836964f4164f
Ali Mora <56822813+AliMora83@users.noreply.github.com>
2025-10-18 10:27:03 +0200

    Remove unnecessary sections from Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,6 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
-import AboutSection from '../components/AboutSection';
-import LegacySection from '../components/LegacySection';
-import MagicYearsSection from '../components/MagicYearsSection';
-import WorkHighlightsSection from '../components/WorkHighlightsSection';
-import WhatWeOfferSection from '../components/WhatWeOfferSection';
-import ClienteleSection from '../components/ClienteleSection';
-import ExperienceStats from '../components/ExperienceStats';
-import ServicesSection from '../components/ServicesSection';
-import ClientsTestimonials from '../components/ClientsTestimonials';
-import TeamSection from '../components/TeamSection';
-import ImpactSection from '../components/ImpactSection';
-import ContactSection from '../components/ContactSection';
 import Footer from '../components/Footer';
 
 const Home = () => {
@@ -20,18 +8,6 @@ const Home = () => {
       <>
         <Navbar />
         <Hero />
-        <AboutSection />
-        <LegacySection />
-        <MagicYearsSection />
-        <WorkHighlightsSection />
-        <WhatWeOfferSection />
-        <ClienteleSection />
-        <ExperienceStats />
-        <ServicesSection />
-        <ClientsTestimonials />
-        <TeamSection />
-        <ImpactSection />
-        <ContactSection />
         <Footer />
       </>
   );
