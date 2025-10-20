f73d33690d87a95c2495e18459efa43315632122
Ali Mora <56822813+AliMora83@users.noreply.github.com>
2025-10-20 10:24:24 +0000

    Font Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,30 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      colors: {
-        primary: '#931e2e',
-        bgMain: '#0F0E0E',
-        textMain: '#f3f4f6',
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+        h1: ['Montserrat', 'sans-serif'],
+        h2: ['Montserrat', 'sans-serif'],
+        h3: ['Montserrat', 'sans-serif'],
+        h4: ['Montserrat', 'sans-serif'],
+        h5: ['Montserrat', 'sans-serif'],
+        h6: ['Montserrat', 'sans-serif'],
+        paragraph: ['Montserrat', 'sans-serif'],
+        link: ['Montserrat', 'sans-serif'],
+      },
+      fontWeight: {
+        'h1': '700',
+        'h2': '600',
+        'h3': '500',
+        'h4': '500',
+        'h5': '500',
+        'h6': '500',
+        'paragraph': '400',
+        'link': '400',
       },
     },
   },
