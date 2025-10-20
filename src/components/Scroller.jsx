147617f6c71bd537c2cff49480ac60f26ef60ae9
Ali Mora <56822813+AliMora83@users.noreply.github.com>
2025-10-20 13:25:15 +0200

    Add black background, larger text, and center content

--- src/components/Scroller.jsx
+++ src/components/Scroller.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-
 const Scroller = () => {
   return (
-    <section className="overflow-hidden w-full mt-8">
-      <div className="animate-horizontal-scroll whitespace-nowrap flex">
-        <p className="text-lg text-gray-400 inline-block px-4">Creating Unforgettable Moments for Your Special Events</p>
-        <p className="text-lg text-gray-400 inline-block px-4">Creating Unforgettable Moments for Your Special Events</p>
-        <p className="text-lg text-gray-400 inline-block px-4">Creating Unforgettable Moments for Your Special Events</p>
+    <section className="overflow-hidden w-full mt-8 bg-black flex justify-center items-center">
+      <div className="animate-horizontal-scroll whitespace-nowrap flex text-center">
+        <p className="text-3xl text-gray-400 inline-block px-4">Creating Unforgettable Moments for Your Special Events</p>
+        <p className="text-3xl text-gray-400 inline-block px-4">Creating Unforgettable Moments for Your Special Events</p>
+        <p className="text-3xl text-gray-400 inline-block px-4">Creating Unforgettable Moments for Your Special Events</p>
       </div>
     </section>
   );
 };
-
 export default Scroller;
