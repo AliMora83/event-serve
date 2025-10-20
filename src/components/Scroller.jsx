3b732d7a040998af3a5dd3ef8c5b9f4a6718d063
Ali Mora <56822813+AliMora83@users.noreply.github.com>
2025-10-20 16:01:39 +0200

    Update Scroller.jsx

--- src/components/Scroller.jsx
+++ src/components/Scroller.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const Scroller = () => {
   return (
     <section className="overflow-hidden w-full bg-bgMain flex justify-center items-center">
-      <div className="animate-horizontal-scroll whitespace-nowrap flex text-center">
+      <div className="animate-horizontal-scroll whitespace-nowrap flex text-center uppercase">
         <p className="text-3xl text-white inline-block px-4">Creating <span className="font-bold text-primary">Unforgettable Moments</span> for Your <span className="font-bold text-primary">Special Events</span></p>
         <p className="text-3xl text-white inline-block px-4">Creating <span className="font-bold text-primary">Unforgettable Moments</span> for Your <span className="font-bold text-primary">Special Events</span></p>
         <p className="text-3xl text-white inline-block px-4">Creating <span className="font-bold text-primary">Unforgettable Moments</span> for Your <span className="font-bold text-primary">Special Events</span></p>
