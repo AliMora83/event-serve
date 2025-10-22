f88b76365b0ba9805ea47a2298b3f5f69744da50
Ali Mora <56822813+AliMora83@users.noreply.github.com>
2025-10-22 16:58:58 +0200

    Update Logo Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Logo from '../assets/Events-01-07.png'; 
+
 const Navbar = () => {
   return (
     <nav className="bg-bgMain shadow-lg">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <div className="text-2xl font-bold text-white">
-            EventServe
+          <img 
+            src={Logo} 
+            alt="EventServe Logo" 
+            className="h-10 w-auto" // adjust height as needed
+          />
           </div>
           <div className="hidden md:flex space-x-8">
             <a href="#home" className="text-gray-300 hover:text-white transition-colors">Home</a>
